f542ead36226013b1abde5ec9cf8e6a0025a070c
mrPlotnik <plotnik1992@gmail.com>
2022-03-16 20:53:42 +0700

    lesson 13 coplete

--- 13_javascript/js/script.js
+++ 13_javascript/js/script.js
@@ -13,43 +13,114 @@ const swiper = new Swiper('.swiper', {
 
 document.addEventListener('DOMContentLoaded', function() {
 
-  // поиск
-  var searchSmall = document.querySelector('.search-small')
-  var searchBig = document.querySelector('.header-nav__search')
-  var btnClose = document.querySelector('.btn--close')
-  var btnSearch = document.querySelector('.btn--search')
+  // Поиск
+  function search() {
+    const searchSmall = document.querySelector('.search-small');
+    const searchBig = document.querySelector('.header-nav__search');
+    const btnClose = document.querySelector('.btn--close');
+    const btnSearch = document.querySelector('.btn--search');
 
-  const toggleMenu = function(q) {
-    q.classList.toggle('open');
+    const toggleSearch = function(q) {
+      q.classList.toggle('open');
+    }
+
+    searchSmall.addEventListener('click', function(e) {
+      e.stopPropagation();
+      toggleSearch(searchBig);
+    })
+
+    btnClose.addEventListener('click', function(e) {
+      toggleSearch(searchBig);
+    })
+
+    btnSearch.addEventListener('click', function(e) {
+      toggleSearch(searchBig);
+    })
+
+    // Отслеживаю клик вне контейнера .searchBig
+    document.addEventListener('click', function(e) {
+      // Определяю место клика
+      const target = e.target;
+      // Клик был на .searchBig и его вложенные элементы или нет?
+      const itsSearch = target == searchBig || searchBig.contains(target);
+      // .searchBig открыт?
+      const searchIsActive = searchBig.classList.contains('open');
+
+      // Если клик был вне .searchBig и .searchBig открыт, то выполняю код
+      if (!itsSearch && searchIsActive) {
+        toggleSearch(searchBig);
+      }
+    });
   }
 
-  searchSmall.addEventListener('click', function(e) {
-    e.stopPropagation();
-    toggleMenu(searchBig);
-  })
+  // Бургер
+  function burger() {
+    const burger = document.querySelector('.site-header__burger-wrap');
+    const menuMobile = document.querySelector('.site-header__menu-mobile');
+    const closeMenu = document.querySelector('.close');
 
-  btnClose.addEventListener('click', function(e) {
-    toggleMenu(searchBig);
-  })
-
-  btnSearch.addEventListener('click', function(e) {
-    toggleMenu(searchBig);
-  })
-
-  // Отслеживаю клик вне контейнера .searchBig
-  document.addEventListener('click', function(e) {
-    // Определяю место клика
-    const target = e.target;
-    // Клик был на .searchBig и его вложенные элементы или нет?
-    const its_menu = target == searchBig || searchBig.contains(target);
-    // .searchBig открыт?
-    const menu_is_active = searchBig.classList.contains('open');
-
-    // Если клик был вне .searchBig и .searchBig открыт, то выполняю код
-    if (!its_menu && menu_is_active) {
-      toggleMenu(searchBig);
+    const toggleMenu = function(q) {
+      q.classList.toggle('open');
     }
+
+    burger.addEventListener('click', function(e) {
+      e.stopPropagation();
+      toggleMenu(menuMobile);
+    })
+
+    closeMenu.addEventListener('click', function(e) {
+      toggleMenu(menuMobile);
+    })
+
+    // Отслеживаю клик вне контейнера .site-header__menu-mobile
+    document.addEventListener('click', function(e) {
+      // Определяю место клика
+      const target = e.target;
+      // Клик был на .site-header__menu-mobile и его вложенные элементы или нет?
+      const itsMenu = target == menuMobile || menuMobile.contains(target);
+      // .site-header__menu-mobile открыт?
+      const menuIsActive = menuMobile.classList.contains('open');
+
+      // Если клик был вне .site-header__menu-mobile и .site-header__menu-mobile открыт, то выполняю код
+      if (!itsMenu && menuIsActive) {
+        toggleMenu(menuMobile);
+      }
+    });
+  }
+
+  // Табы секции .work
+  function tab() {
+
+    const tabsNavBtn = document.querySelectorAll('.tab-nav__btn');
+    const tabsContent = document.querySelectorAll('.tab-content');
+
+    tabsNavBtn.forEach(function(el) {
+
+      el.addEventListener('click', function(e) {
+
+        const path = e.currentTarget.dataset.path;
+
+        tabsContent.forEach(function(el) {
+          el.classList.remove('tab-content-active');
+        })
+
+        document.querySelector(`[data-target="${path}"]`).classList.add('tab-content-active');
+
+      })
+
+    })
+
+  }
+
+  search();
+  burger();
+  tab();
+
+  $("#accordion").accordion({
+    icons: false,
+    heightStyle: "content",
+    collapsible: true
   });
 
-
 })
+
