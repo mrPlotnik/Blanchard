ba544d22ecee5f9ccf2ab3ac1d438b19c2e35077
Andrey <plotnik1992@gmail.com>
2022-05-23 11:06:53 +0700

    more fixes

--- 16_knowledge-base/js/script.js
+++ 16_knowledge-base/js/script.js
@@ -1,139 +1,33 @@
-const swiper = new Swiper('.swiper', {
-  // loop: true,
-  effect: "fade",
-  allowTouchMove: false,
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: true,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  a11y: {
-    paginationBulletMessage: 'Слайд {{index}}',
+// ---------- Кастомизация инпута
 
-  },
+const el = document.querySelector('select');
+const choises = new Choices(el,{
+  searchEnabled: false,
+  itemSelectText: '',
+  allowHTML: true,
+ 
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+// ---------- Yandex-карты
 
-  // Поиск
-  function search() {
-    const searchSmall = document.querySelector('.search-js');
-    const searchBig = document.querySelector('.header-nav__search');
-    const btnClose = document.querySelector('.btn-close-js');
-    const btnSearch = document.querySelector('.btn-search-js');
-
-    const toggleSearch = function(q) {
-      q.classList.toggle('open');
-    }
-
-    searchSmall.addEventListener('click', function(e) {
-      e.stopPropagation();
-      toggleSearch(searchBig);
-    })
-
-    btnClose.addEventListener('click', function(e) {
-      toggleSearch(searchBig);
-    })
-
-    btnSearch.addEventListener('click', function(e) {
-      toggleSearch(searchBig);
-    })
-
-    // Отслеживаю клик вне контейнера .searchBig
-    document.addEventListener('click', function(e) {
-      // Определяю место клика
-      const target = e.target;
-      // Клик был на .searchBig и его вложенные элементы или нет?
-      const itsSearch = target == searchBig || searchBig.contains(target);
-      // .searchBig открыт?
-      const searchIsActive = searchBig.classList.contains('open');
-
-      // Если клик был вне .searchBig и .searchBig открыт, то выполняю код
-      if (!itsSearch && searchIsActive) {
-        toggleSearch(searchBig);
-      }
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init(){
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+        // Координаты центра карты.     
+        center: [48.872185, 2.354224],
+        // Уровень масштабирования. От 0 (весь мир) до 19.
+        zoom: 15
     });
-  }
+   
+    var myPlacemark = new ymaps.Placemark([48.872185, 2.354224], {}, {
+      iconLayout: 'default#image',
+      iconImageHref: '../img/map-point.svg',
+      iconImageSize: [28, 40]
+    });  
 
-  // Бургер
-  function burger() {
-    const burger = document.querySelector('.site-header__burger-link');
-    const menuMobile = document.querySelector('.menu-mobile');
-    const closeMenu = document.querySelector('.close');
-
-    const toggleMenu = function(q) {
-      q.classList.toggle('open');
-    }
-
-    burger.addEventListener('click', function(e) {
-      e.stopPropagation();
-      toggleMenu(menuMobile);
-    })
-
-    closeMenu.addEventListener('click', function(e) {
-      toggleMenu(menuMobile);
-    })
-
-    // Отслеживаю клик вне контейнера .site-header__menu-mobile
-    document.addEventListener('click', function(e) {
-      // Определяю место клика
-      const target = e.target;
-      // Клик был на .site-header__menu-mobile и его вложенные элементы или нет?
-      const itsMenu = target == menuMobile || menuMobile.contains(target);
-      // .site-header__menu-mobile открыт?
-      const menuIsActive = menuMobile.classList.contains('open');
-
-      // Если клик был вне .site-header__menu-mobile и .site-header__menu-mobile открыт, то выполняю код
-      if (!itsMenu && menuIsActive) {
-        toggleMenu(menuMobile);
-      }
-    });
-  }
-
-  // Табы секции .work
-  function tab() {
-
-    const tabsNavBtn = document.querySelectorAll('.tab-nav__btn');
-    const tabsContent = document.querySelectorAll('.tab-content');
-
-    tabsNavBtn.forEach(function(el) {
-
-      el.addEventListener('click', function(e) {
-
-        const path = e.currentTarget.dataset.path;
-
-        tabsContent.forEach(function(el) {
-          el.classList.remove('tab-content-active');
-        })
-
-        document.querySelector(`[data-target="${path}"]`).classList.add('tab-content-active');
-
-      })
-
-    })
-
-  }
-
-  search();
-  burger();
-  tab();
-
-  // Чекбокс по нажатию на Enter
-  $('input:checkbox').keypress(function(e) {
-    e.preventDefault();
-    if((e.keyCode ? e.keyCode : e.which) == 13) {
-      $(this).trigger('click');
-    }
-  });
-
-  // Аккордион
-  $("#accordion").accordion({
-    icons: false,
-    heightStyle: "content",
-    collapsible: true
-  });
-
-})
+    // Размещение геообъекта на карте.
+    myMap.geoObjects.add(myPlacemark);
+}
